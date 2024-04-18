fb00392b61ffd5008e3469b2e01ddc2582399457
Pinku <kpinku32@gmail.com>
2024-04-18 11:45:00 +0530

    src and pom files added

---
+++ src/main/java/com/pinku/aws/rds/api/SpringbootAwsRdsApplication.java
@@ -0,0 +1,13 @@
+package com.pinku.aws.rds.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsRdsApplication.class, args);
+	}
+
+}
